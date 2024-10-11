7c9f7d9595aa37c1345240a54d26dbac9cab5832
PalesaSindiswa <tsotetsipalesa38@gmail.com>
2024-10-11 14:37:45 +0200

    Waste Management Assessment

---
+++ src/test/java/com/enviro/assessment/inter001/PalesaTsotetsi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.inter001.PalesaTsotetsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
